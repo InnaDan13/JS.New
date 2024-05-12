aa910fb85b2b8db405b3430ba1bba64fb924fba6
Inna Danylchuk <innadanylchuk13@gmail.com>
2024-05-12 20:54:42 +0200

    add color

--- assets/js/conditions.js
+++ assets/js/conditions.js
@@ -8,8 +8,25 @@ const a = 1;
 const b = 10;
 console.log(`${a} + ${b} = ${a+b}`);
 
-const isOnline = true;
+const isOnline = false;
+let color = null;
+
+if(isOnline){
+     color = 'green';
+} else { color = 'red';}
+console.log(color);
+
 
 if (isOnline){
-    
+    document.write(`<p>User is <span style = "color:${color}">online </span></p>`)
+} else {
+    document.write(`<p>User is <span style = "color:${color}> offline</span></p>`)
+}
+
+const isMoney = false;
+
+if (isMoney){
+    document.write(`<p>Accepted</p>`)
+} else {
+    document.write(`<p>Declined</p>`)
 }
